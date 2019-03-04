741dc0404082e2232c01eba174b8d5908430694c
SamBritt <sam.britt2@yahoo.com>
2019-03-04 10:50:28 -0600

    introduced to creating html elements in javascript. Beginning to make code modular

---
+++ src/scripts/main.js
@@ -0,0 +1,110 @@
+const article = document.querySelector('#container');
+
+const students = [
+    {
+        name: "Chris Miller",
+        subject: "History",
+        info: "Failed last exam",
+        score: 59
+    },
+    {
+        name: "Courtney Seward",
+        subject: "History",
+        info: "Has completed all homework",
+        score: 91
+    },
+    {
+        name: "Garrett Ward",
+        subject: "History",
+        info: "Wonderful at helping other students",
+        score: 88
+    },
+    {
+        name: "John Dulaney",
+        subject: "History",
+        info: "Has never missed a class or exam",
+        score: 92
+    },
+    {
+        name: "Greg Lawrence",
+        subject: "History",
+        info: "Sub-par performance all around",
+        score: 64
+    },
+    {
+        name: "Leah Duvic",
+        subject: "History",
+        info: "Wonderful student",
+        score: 97
+    },
+    {
+        name: "Jesse Page",
+        subject: "History",
+        info: "Smokes too much. Distracting.",
+        score: 76
+    },
+    {
+        name: "Kevin Haggerty",
+        subject: "History",
+        info: "Falls asleep in class",
+        score: 79
+    },
+    {
+        name: "Max Wolf",
+        subject: "History",
+        info: "Talks too much",
+        score: 83
+    },
+    {
+        name: "Lissa Goforth",
+        subject: "History",
+        info: "Asks pointless, unrelated questions",
+        score: 78
+    },
+    {
+        name: "Tyler Bowman",
+        subject: "History",
+        info: "When was the last time he attended class?",
+        score: 48
+    },
+    {
+        name: "Ray Medrano",
+        subject: "History",
+        info: "Needs to contribute to in-class discussions",
+        score: 95
+    }
+]
+
+const createStudentComponent = (name, subject, info, className) => {
+    return `
+        <div class="student ${className}">
+            <h1>${name}</h1>
+            <section>${subject}</section>
+            <aside>
+                ${info}
+            </aside>
+        </div>
+    `
+}
+
+for(let i = 0; i < students.length; i++){
+
+    if(students[i].score >= 70){
+        article.innerHTML += createStudentComponent(
+            students[i].name, 
+            students[i].subject, 
+            students[i].info,
+            "passing"
+            );
+    }else{
+        article.innerHTML += createStudentComponent(
+            students[i].name, 
+            students[i].subject, 
+            students[i].info,
+            "failing"
+            );
+    }
+}
+
+
+
